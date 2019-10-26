6879706ef72df56c1f3e3784d6703c7ae374c8bd
Emily Vogel <eav990@gmail.com>
2019-10-26 17:15:08 -0400

    Add coding logic from notes

--- src/program/Client.java
+++ src/program/Client.java
@@ -3,13 +3,8 @@ package program;
 public class Client {
     public static void main(String[] args) {
         try {
-            // NOTE: You can either point to a new file location or modify the existing txt file
-            Tokenizer tokenizer = new Tokenizer("src/resources/input.txt");
-            while (!tokenizer.kind().equals("end-of-text")) {
-                tokenizer.next();
-                System.out.println("\n" + tokenizer.position());
-                System.out.println("< " + tokenizer.kind() + ", " + tokenizer.value() + " >");
-            }
+            Parser parser = new Parser();
+            parser.run();
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/program/Parser.java
+++ src/program/Parser.java
@@ -4,53 +4,140 @@ import java.util.List;
 
 public class Parser {
 
-    private String currentSymbol;
+    Tokenizer tokenizer;
 
-    public void expression() {
-
+    public Parser() throws Exception {
+        tokenizer = new Tokenizer("src/resources/input.txt");
     }
 
-    public void booleanExpression() {
-
+    public Parser(String file_loc) throws Exception {
+        tokenizer = new Tokenizer(file_loc);
     }
 
-    public void booleanTerm() {
+    public void run() throws Exception {
+        nextSymbol();
+        expression();
 
+        if (!tokenizer.kind().equals("end-of-text")) {
+            // TODO use report error function
+            throw new Exception("Found symbol " + tokenizer.kind() + " at line " + tokenizer.position() + " expected end-of-text");
+        }
     }
 
-    public void booleanFactor() {
-
+    public void expression() throws Exception {
+        booleanExpression();
     }
 
-    public void booleanLiteral() {
+    public void booleanExpression() throws Exception {
+        booleanTerm();
 
+        while (isIn(new String[]{"or"})) {
+            nextSymbol();
+            booleanTerm();
+        }
     }
 
-    public void arithmeticExpression() {
+    public void booleanTerm() throws Exception {
+        booleanFactor();
 
+        while (isIn(new String[]{"and"})) {
+            nextSymbol();
+            booleanFactor();
+        }
     }
 
-    public void term() {
+    public void booleanFactor() throws Exception {
+        if (isIn(new String[]{"not"})) {
+            nextSymbol();
+        }
 
+        arithmeticExpression();
+
+        if (isIn(new String[]{"=", "<"})) {
+            nextSymbol();
+            arithmeticExpression();
+        }
     }
 
-    public void factor() {
-
+    public void booleanLiteral() throws Exception {
+        if (isIn(new String[]{"true", "false"})) {
+            nextSymbol();
+        } else {
+            throw new Exception("Expected: true, false");
+        }
     }
 
-    public void literal() {
+    public void arithmeticExpression() throws Exception{
+        term();
 
+        while (isIn(new String[]{"+", "-"})) {
+            nextSymbol();
+            term();
+        }
     }
 
-    public void accept(String symbol) {
+    public void term() throws Exception {
+        factor();
 
+        while (isIn(new String[]{"*", "/"})) {
+            nextSymbol();
+            term();
+        }
     }
 
-    public void expected(List<String> symbols) {
-
+    public void factor() throws Exception {
+        if (isIn(new String[]{"true", "false", "NUM"})) {
+            literal();
+        } else if (isIn(new String[]{"ID"})) {
+            nextSymbol();
+        } else if (isIn(new String[]{"("})) {
+            nextSymbol();
+            expression();
+            accept(")");
+        } else {
+            // TODO move this call to it's own method
+            throw new Exception("Expected: true, false, NUM, ID, or ( instead got " + tokenizer.kind());
+        }
     }
 
-    public void nextSymbol() {
+    public void literal() throws Exception {
+        if (isIn(new String[]{"true", "false", "NUM"})) {
+            if (isIn(new String[]{"NUM"})) {
+                nextSymbol();
+            } else {
+                booleanLiteral();
+            }
+        } else {
+            throw new Exception("Expected: true, false, NUM");
+        }
+    }
 
+    public void accept(String symbol) throws Exception {
+        if (tokenizer.kind().equals(symbol)) {
+            nextSymbol();
+        } else {
+            // TODO move this call to it's own method
+            throw new Exception("Expected " + symbol + " seeing " + tokenizer.kind());
+        }
+    }
+
+    public void expected(List<String> symbols) throws Exception {
+        if (!isIn((String[]) symbols.toArray())) {
+            throw new Exception("Expected " + symbols + " seeing " + tokenizer.kind());
+        }
+    }
+
+    public void nextSymbol() throws Exception{
+        if (!isIn(new String[]{"end-of-text"})) {
+            tokenizer.next();
+        }
+    }
+
+    public boolean isIn(String[] symbols) throws Exception {
+        for (String symbol: symbols) {
+            if (symbol.equals(tokenizer.kind()))
+                return true;
+        }
+        return false;
     }
 }
