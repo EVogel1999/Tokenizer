681a54458548c6a7515e1e8ba4117badf316175e
Emily Vogel <eav990@gmail.com>
2019-10-26 15:38:00 -0400

    Generate skeleton code

---
+++ src/program/Parser.java
@@ -0,0 +1,56 @@
+package program;
+
+import java.util.List;
+
+public class Parser {
+
+    private String currentSymbol;
+
+    public void expression() {
+
+    }
+
+    public void booleanExpression() {
+
+    }
+
+    public void booleanTerm() {
+
+    }
+
+    public void booleanFactor() {
+
+    }
+
+    public void booleanLiteral() {
+
+    }
+
+    public void arithmeticExpression() {
+
+    }
+
+    public void term() {
+
+    }
+
+    public void factor() {
+
+    }
+
+    public void literal() {
+
+    }
+
+    public void accept(String symbol) {
+
+    }
+
+    public void expected(List<String> symbols) {
+
+    }
+
+    public void nextSymbol() {
+
+    }
+}
